5ef9389cefc9f979b3fe8db3778bebf757f58138
HudgeW <61322903+HudgeW@users.noreply.github.com>
2022-03-29 13:23:26 +0200

    Co-authored-by: Elsa Kidman <Elki3276@users.noreply.github.com>

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import theme from '../frontend/theme';
+import {ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+  );
 }
 
 export default MyApp
