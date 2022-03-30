038dee4ba7fa488e1e85c3cb66d5b118b9c8f772
Elsa <Elsa@kidmans.com>
2022-03-30 11:35:21 +0200

    frontpage

---
+++ app/pages/frontpage.js
@@ -0,0 +1,13 @@
+import Header from '../frontend/Header';
+import Posts from '../frontend/Posts';
+
+
+export default function FrontPage() {
+    return (
+      <><div className="">
+        <Header />
+      </div><div className="">
+          <Posts />
+        </div></>
+    );
+  }
