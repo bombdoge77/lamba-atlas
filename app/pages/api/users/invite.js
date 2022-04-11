1ce5ed3125381b2eaf48f5382c2b4edb30cded4e
Oliver Kiss <oliver.robert1999@gmail.com>
2022-04-11 11:03:02 +0200

    sketch api route for invite codes and backend function for invite code validation

---
+++ app/pages/api/users/invite.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+import { connect, disconnect } from '../../../utils/db/db.js'
+
+export default async function regHandler(req, res) {
+  if (req.method == 'GET') {
+    await gen_code(req, res)
+  } else if (req.method == 'POST') {
+    validate_code(req, res)
+  }
+}
+
+async function gen_code(req, res) {
+  return '123456'
+}
+
+async function validate_code(req, res) {
+  return true
+}
--- app/utils/db/users.js
+++ app/utils/db/users.js
@@ -67,32 +67,39 @@ export async function auth(db, email, pass) {
 	return bcrypt.compareSync(pass, expected_hash)
 }
 
-function clear_invite_codes(db) {
+async function clear_invite_codes(db) {
 	var codes = db.collection('invite_codes')
 	var today = new Date()
-	codes.deleteMany({ expires : { $lt : today.parse() } })
+	await codes.deleteMany({ expires : { $lt : today.parse() } })
 }
 
 export async function gen_invite_code(db) {
 	var done = false
-	clear_invite_codes(db)
-	
+	await clear_invite_codes(db)
+
 	while (!done) {
 			crypto.randomBytes(48, (err, buf) => {
-			if (err) throw err
+				if (err) throw err
 
-			var code = buf.toString('hex')
-			var codes = db.collection('invite_codes')
-			var code_exists = (codes.findOne({ code : code }) != null)
-			if (!code_exists) {
-				var today = new Date()
-				var tomorrow = new Date(today)
-				tomorrow.setDate(tomorrow.getDate + 1) // increment by one day
-				await codes.insertOne({ code : code, expires : tomorrow.parse() })
-				done = true
-			}
+				var code = buf.toString('hex')
+				var codes = db.collection('invite_codes')
+				var code_exists = (codes.findOne({ code : code }) != null)
+				if (!code_exists) {
+					var today = new Date()
+					var tomorrow = new Date(today)
+					tomorrow.setDate(tomorrow.getDate + 1) // increment by one day
+					await codes.insertOne({ code : code, expires : tomorrow.parse() })
+					done = true
+				}
 		});
 	}
 
 	return code
+}
+
+export async function validate_invite_code(db) {
+	var codes = db.collection('invite_codes')
+	var date = new Date()
+	var result = await codes.findOne({ code : code, expires : { $gt : date.parse() } })
+	return result != null
 }
