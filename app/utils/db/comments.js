df41b294e6bc2cb79e62d689437762535f0e68dc
Agnes <agnessidemo@hotmail.com>
2022-04-20 11:55:38 +0200

    sketch of comment functions

--- app/utils/db/comments.js
+++ app/utils/db/comments.js
@@ -2,18 +2,46 @@
 // each comment must have an id
 // save list of comment ids in user profile
 
-export async function make_comment(db, user, post, comment) {
+export async function make_comment(db, user, post, text, is_reply) {
+	var comments = db.collection('comments')
+    var comment = {
+        post_id : post,
+        body : text,
+        user : user,
+        likes : 0,
+        replies : [], //lagra alla id för kommentarer som svarar på denna
+        is_reply : is_reply
+        //Date
+    }
 
+	// error handling
+	// store pictures?
+	var result = await comments.insertOne(comment)
+	return result.acknowledged
 }
 
 export async function delete_comment(db, comment_id) {
+	var comments = db.collection('comments')
+
+	// no error handling needed?
+	var result = await comments.deleteOne({ _id : comment_id })
+	return result.acknowledged 
 
 }
 
-export async function edit_comment(db, comment_id, new_comment) {
+export async function edit_comment(db, comment_id, new_text) {
+	var comments = db.collection('comments')
 
+	var result = await comments.updateOne({ _id : comment_id }, { $set : {body : new_text} })
+	return result.ok
 }
 
 export async function get_comment(db, comment_id) {
+	var comments = db.collection('comments')
 
+	var result = await comments.findOne({ _id : comment_id })
+	return result
 }
+
+
+//Ersätta ett fält i ett objekt
